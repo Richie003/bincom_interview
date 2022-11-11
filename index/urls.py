2c0f0e1755923ee87f090948e9e6c44bb0cff3ad
Richie003 <richiedilosi2003@gmail.com>
2022-11-11 11:59:05 +0100

    finalised form submiting with AJAX & jQuery

--- index/urls.py
+++ index/urls.py
@@ -4,5 +4,6 @@ from index import views
 urlpatterns = [
     path('', views.ResultIndex, name='home'),
     path('lga/', views.get_lga, name='get_LGA'),
-    path('Party-result/', views.PartyIndex, name='PartyResult')
+    path('Party-result/', views.PartyIndex, name='PartyResult'),
+    path('add-party-result/', views.addPartyResult, name='apres')
 ]
--- index/views.py
+++ index/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, JsonResponse
+import datetime
 from django.shortcuts import render
 from .models import *
 from .forms import *
@@ -28,4 +29,24 @@ def PartyIndex(request, *args, **kwargs):
         'form':form,
          'party_data': party_results
     }
-    return render(request, 'example2.html', context)
+    return render(request, 'party_result.html', context)
+
+def addPartyResult(request):
+    if request.method == "POST":
+        id_data = request.POST.get('puu')
+        party_abbr = request.POST.get('pa')
+        party_score_data = request.POST.get('ps')
+        user = request.POST.get('user')
+        date = datetime.datetime.now()
+        ipaddr = request.POST.get('ipadd')
+        create_result = Announced_pu_results.objects.create(
+            polling_unit_uniqueid = id_data,
+            party_abbreviation = party_abbr,
+            party_score=party_score_data,
+            entered_by_user = user,
+            date_entered = date,
+            user_ip_address = ipaddr
+        )
+        create_result.save()
+        success = f'Result added successfully for {party_abbr} by {user} on {date.date()}'
+    return HttpResponse(success)
